e95aaf6543932cf88e3b25bfd3e6425ea9fc2346
Leoman <leo.pinzon102@gmail.com>
2022-07-15 17:38:41 -0400

    Add heroku

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ function filterByQuery(query, animalsArray){
 
     return filteredResults;
 };
+//function to find an unique animal based on the id
+function findById(id, animalsArray){
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 
 //add data route
 app.get('/api/animals', (req, res) => {
@@ -58,6 +64,18 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results)
     }
     res.json(results);
+});
+
+//create a new get for animal ids from parameters
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    //add 404 error if the id is not in the data
+    if (result){
+        res.json(result);
+    } else {
+        //res.sendStatus(status)
+        res.send(404)
+    }
 });
 
 //listen the express method
